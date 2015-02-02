08be83fc2b42b84c060f438c467802836eaa99a5
Steve Kinney <hello@stevekinney.net>
2015-02-02 15:41:41 -0700

    Adjust maximum stock; add dynamic charts

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,5 +2,21 @@ class StocksController < ApplicationController
   def index
     @stocks = Stock.all
     @year = current_year
+    @date = current_date
+  end
+
+  private
+
+  def current_date
+    case current_year
+    when 2000
+      "2000-01-03"
+    when 2002
+      "2002-09-23"
+    when 2007
+      "2007-10-01"
+    else
+      nil
+    end
   end
 end
