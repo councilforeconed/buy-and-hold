a4b9676fa4a7bfd4133093627c16c310099dd0be
Steve Kinney <hello@stevekinney.net>
2015-06-22 14:07:12 -0700

    Adjust bond prices

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 
-Stock.create(company: "Bonds", symbol: "Bonds", initial_value: 1, price_2002: 1.08, price_2007: 1.26)
+Stock.create(company: "Bonds", symbol: "Bonds", initial_value: 50, price_2002: 54, price_2007: 63)
 
 YAML.load(File.read('./data/stocks.yml')).each do |stock|
   Stock.create(stock)
